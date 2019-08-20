6752c9382d761a72f7c5891c8d835530295be7ff
Hulian2000 <prepmoringa11@gmail.com>
2019-08-20 17:16:02 +0300

    add files to quote details

--- src/app/my-quote-details/my-quote-details.component.ts
+++ src/app/my-quote-details/my-quote-details.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '@angular/compiler';
 @Component({
-  selector: 'app-my-quote-details',
-  templateUrl: './my-quote-details.component.html',
-  styleUrls: ['./my-quote-details.component.css']
+ selector: 'app-my-quote-details',
+ templateUrl: './my-quote-details.component.html',
+ styleUrls: ['./my-quote-details.component.css']
 })
 export class MyQuoteDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+ @Input() quote:Quote;
+ @Output() isDelete=new EventEmitter<boolean>();
+ quoteDelete(Delete:boolean){
+   this.isDelete.emit(Delete);
+ }
+ constructor() { }
+ ngOnInit() {
+ }
 }
