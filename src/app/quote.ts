8923f7b4ca02f653b0cbd2e0a079a432a46f80b8
Hulian2000 <prepmoringa11@gmail.com>
2019-08-20 17:10:54 +0300

    add files

---
+++ src/app/quote.ts
@@ -0,0 +1,13 @@
+export class Quote {
+    showDetails:boolean;
+    upvotes:number;
+    downvotes:number;
+    constructor(public id:number,public name:string,public author:string,public submittedBy:string,public datecreation:Date){
+        this.showDetails=false;
+        this.upvotes=0;
+        this.downvotes=0;
+    }
+}
+
+
+
