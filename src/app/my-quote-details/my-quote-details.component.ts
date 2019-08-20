3020983362ea4ecacccb95da8bbe81e29cb52753
Hulian2000 <prepmoringa11@gmail.com>
2019-08-20 17:12:23 +0300

    add my quote details

---
+++ src/app/my-quote-details/my-quote-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-quote-details',
+  templateUrl: './my-quote-details.component.html',
+  styleUrls: ['./my-quote-details.component.css']
+})
+export class MyQuoteDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
