7a187173434a7a648986ae17c1755e457a0f71d3
Hulian2000 <prepmoringa11@gmail.com>
2019-08-22 10:04:56 +0300

    add files

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,25 @@
 export class Quote {
+    public id:number;
+    public name:string;
+    public author:string;
+    public submittedBy:string;
+    public dateCreation:Date;
     showDetails:boolean;
     upvotes:number;
     downVotes:number;
-    constructor(public id:number,public name:string,public author:string,public submittedBy:string,public datecreation:Date){
+
+    constructor(
+        id:number,
+        name:string,
+        author:string,
+        submittedBy:string,
+        dateCreation:Date
+    ){
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.submittedBy = submittedBy;
+        this.dateCreation = dateCreation;
         this.showDetails=false;
         this.upvotes=0;
         this.downVotes=0;
